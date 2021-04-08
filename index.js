868744ba2bacab2fd7cb9e19c1ec0c9454fbcc44
Mitzi17 <m_estrada17@live.com>
2021-04-08 11:45:23 -0700

    commit tree 376226ffccb26ddc847f415d248f78d1d2989d3c

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Your code here
+
+// function declaration with default parameter
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+// function expression with default parameter
+const mondayWork = function(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+const wrapAdjective = function(outerStr="*") {
+    return function(innerStr="special") {
+        return `You are ${outerStr}${innerStr}${outerStr}!`
+    }   
+}
+
+const Calculator = { 
+    add: function(a,b) {
+        return a + b
+      },
+    
+    subtract: function(a,b) {
+        return a - b
+    },
+
+    multiply: function(a,b) {
+        return a * b
+    },
+
+    divide: function(a,b) {
+        return a/b
+    }
+}
+
+function actionApplyer(integer, array) {
+    let s = integer
+    
+    for (let i = 0; i < array.length; i++){
+        s = array[i](s)
+    }
+    return s
+}
+
+
